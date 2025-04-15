a5454c429ea5c19aa097a204b65eb57dcae9b462
Anderson Passos <anderson.passos@yahoo.com.br>
2025-04-15 19:47:00 -0300

    Repositório inicial do motoclub-service

---
+++ src/main/java/com/motoclub/motoclub_service/MotoclubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.motoclub.motoclub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoclubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoclubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motoclub/motoclub_service/MotoclubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoclub.motoclub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoclubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
